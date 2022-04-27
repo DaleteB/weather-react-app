0955e1f9e6c521b6e128be93711884985c02fb4d
Dalete Pokrywiesky <95585407+DaleteB@users.noreply.github.com>
2022-04-27 17:54:16 +0100

    created css file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <div className="container">
        <Weather /> 
+       <footer>
+        This project was coded by <a href="https://bespoke-bienenstitch-78d06f.netlify.app/" target="_blank">Dalete Pokrywiesky</a> and is <a href="https://github.com/DaleteB/weather-react-app" target="_blank">open-source on GitHub</a>
+      </footer>
       </div>
       
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -3,14 +3,14 @@ import "./Weather.css";
 
 export default function weather(){
     return(
-        <div className="weather">
+        <div className="Weather">
             <form>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control" />
+                        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/>
                     </div>
                     <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary" />
+                        <input type="submit" value="Search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
@@ -25,8 +25,13 @@ export default function weather(){
             </ul>
             <div className="row">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="sunny" />
-                6°C
+                  <div className="clearfix">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="sunny" className="float-left"/>
+                  </div>
+                  <div className="float-left">
+                   <span className="temperature">6</span>
+                    <span className="unit">°C</span> 
+                  </div>
                 </div>
                 <div className="col-6">
                     <ul>
